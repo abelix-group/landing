76fe9f32e2b75ca8a8e403da0a1e9869313da3c8
Nurbol Sarsenbayev <nurbol.proger@gmail.com>
2024-03-16 12:39:41 +0500

    abelix.kz

---
+++ precache-manifest.5a4d55e90e8a9ffec799f458f88f26f1.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a896aefb8d1638c81eba969eb667d05",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b99ce6a60d9b95f67ff2",
+    "url": "/static/css/2.c6775b42.chunk.css"
+  },
+  {
+    "revision": "b99ce6a60d9b95f67ff2",
+    "url": "/static/js/2.56f58a18.chunk.js"
+  },
+  {
+    "revision": "fabefd8cf67d620c7f0e",
+    "url": "/static/js/3.e4a0549d.chunk.js"
+  },
+  {
+    "revision": "e786d4e700fdf463f87e",
+    "url": "/static/js/main.13df60e7.chunk.js"
+  },
+  {
+    "revision": "d65efab4f8a79d16dbb7",
+    "url": "/static/js/runtime-main.72561f28.js"
+  },
+  {
+    "revision": "7e3e042d6ddc6fe0dab76bb2288c9490",
+    "url": "/static/media/avatar-3.7e3e042d.png"
+  },
+  {
+    "revision": "c8f9977af8ce40106bcabc8b2775eee6",
+    "url": "/static/media/avatar-5.c8f9977a.png"
+  },
+  {
+    "revision": "9c3003444d3f89f1d13581dca6c3345c",
+    "url": "/static/media/avatar-6.9c300344.png"
+  },
+  {
+    "revision": "6a4f6e820660c39587f01c36ed573e06",
+    "url": "/static/media/avatar-7.6a4f6e82.png"
+  },
+  {
+    "revision": "5b32b51ef4e4aef22708aff435bc62b5",
+    "url": "/static/media/desert-dark.5b32b51e.svg"
+  },
+  {
+    "revision": "5de2149e427da3b67836f13ba2538c17",
+    "url": "/static/media/desert-light.5de2149e.svg"
+  },
+  {
+    "revision": "804728548e43478dff3b1739b1cce70f",
+    "url": "/static/media/download_appstore.80472854.png"
+  },
+  {
+    "revision": "2fab5ce224806fdd04e86b113265b3b1",
+    "url": "/static/media/download_playstore.2fab5ce2.png"
+  },
+  {
+    "revision": "7425d892f93950d95d8b35db8652a3d7",
+    "url": "/static/media/iphone-main.7425d892.png"
+  },
+  {
+    "revision": "31b333398edfc20222be347590b7523e",
+    "url": "/static/media/jsui.31b33339.jpg"
+  },
+  {
+    "revision": "566eb12fcd78cc4385533fbf04dd07be",
+    "url": "/static/media/kitze.566eb12f.jpg"
+  },
+  {
+    "revision": "75f0bde90710fdcd07e34c999d6663fd",
+    "url": "/static/media/sizzy.75f0bde9.jpg"
+  },
+  {
+    "revision": "917926c359a9a66ac36257a3eb06446f",
+    "url": "/static/media/twizzle.917926c3.png"
+  }
+]);
